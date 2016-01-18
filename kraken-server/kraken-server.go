f4235e9cc33342b2d93e7f5e1e4a6d770c726db5
Jan Halfar <jan@bestbytes.com>
2016-01-18 16:41:54 +0100

    added version

--- kraken-server/kraken-server.go
+++ kraken-server/kraken-server.go
@@ -5,16 +5,27 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/foomo/kraken"
 )
 
+const version = "0.3"
+
+var flagVersion = flag.Bool("version", false, "display version")
 var flagAddress = flag.String("address", "127.0.0.1:8888", "where to listen too like 127.0.0.1:8888")
 var flagInsecure = flag.Bool("insecure", false, "disabled by default - controls whether a client verifies the server's certificate chain and host name")
 
 func main() {
 	flag.Parse()
+
+	if *flagVersion {
+		fmt.Println("version", version)
+		os.Exit(2)
+	}
+
 	fmt.Println("I AM KRAKEN at :", *flagAddress)
+
 	if *flagInsecure {
 		fmt.Println("!!! not verifying peers !!!")
 		http.DefaultClient.Transport = &http.Transport{
